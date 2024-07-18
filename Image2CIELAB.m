6d14d694d75f407cdb860f1a04e757d9a8d541ee
Semin <ohsemin1990@gmail.com>
2024-07-18 11:34:04 +0200

    Now using the function from Psychtoolbox for all calculations

--- Image2CIELAB.m
+++ Image2CIELAB.m
@@ -14,9 +14,9 @@ clear; close all;
 displayType = 'sRGB';
 switch displayType
     case 'sRGB'
-        % We'll draw the color gamut of the display later on. 
+        % We'll draw the color gamut of the display later on.
         xyY_targetDisplay = [0.6400 0.3000 0.1500; 0.3300 0.6000 0.0600; 0.2126 0.7152 0.0722];
-        
+
         % 3x3 matrix to convert from the linear RGB to CIE XYZ.
         M_RGB2XYZ = [0.4124 0.3576 0.1805; 0.2126 0.7152 0.0722; 0.0193 0.1192 0.9505];
 
@@ -40,7 +40,7 @@ verbose = true;
 %
 % Here, we are using the test image as it is, when we do it for Coco
 % dataset, we believe the object would have been segmented. Make sure you
-% are in the directory where the image is saved. 
+% are in the directory where the image is saved.
 image = imread('orange.png');
 
 % Display the image if you want.
@@ -74,57 +74,74 @@ XYZ_testImage = M_RGB2XYZ * LRGB;
 xyY_testImage = XYZToxyY(XYZ_testImage);
 
 %% CIE XYZ to CIELAB.
-whitepoint = sum(M_RGB2XYZ,2)';
-lab = xyz2lab(XYZ_testImage',whitepoint);
-lab_chroma(:,1) = sqrt(lab(:,2).^2+lab(:,3).^2);
+whitepoint = sum(M_RGB2XYZ,2);
+lab = XYZToLab(XYZ_testImage,whitepoint);
+lab_chroma(1,:) = sqrt(lab(2,:).^2+lab(3,:).^2);
+
+%% Pick representative color of the test image.
+%
+% Here, we calculate the some color points that would be possible to
+% represent the test image.
+
+% Average.
+XYZ_avgXYZ = mean(XYZ_testImage,2);
+LAB_avgLAB = mean(lab);
+
+% Highest chroma.
+highestChromaVal = max(lab_chroma);
+idx_highestChroma = find(lab_chroma==highestChromaVal);
+XYZ_highestChroma = XYZ_testImage(:,idx_highestChroma);
+LAB_highestChroma = lab(:,idx_highestChroma);
 
 %% Plot the results.
-subplot(2,2,2);
-hold on;
-plot(xyY_testImage(1,:),xyY_testImage(2,:),'r.');
+if (verbose)
+    subplot(2,2,2);
+    hold on;
+    plot(xyY_testImage(1,:),xyY_testImage(2,:),'r.');
 
-% Display gamut. For now, it's set to sRGB for convenience.
-plot([xyY_targetDisplay(1,:) xyY_targetDisplay(1,1)], [xyY_targetDisplay(2,:) xyY_targetDisplay(2,1)],'k-','LineWidth',1);
+    % Display gamut. For now, it's set to sRGB for convenience.
+    plot([xyY_targetDisplay(1,:) xyY_targetDisplay(1,1)], [xyY_targetDisplay(2,:) xyY_targetDisplay(2,1)],'k-','LineWidth',1);
 
-% Plackian locus.
-load T_xyzJuddVos
-T_XYZ = T_xyzJuddVos;
-T_xy = [T_XYZ(1,:)./sum(T_XYZ); T_XYZ(2,:)./sum(T_XYZ)];
-plot([T_xy(1,:) T_xy(1,1)], [T_xy(2,:) T_xy(2,1)], 'k-');
+    % Plackian locus.
+    load T_xyzJuddVos
+    T_XYZ = T_xyzJuddVos;
+    T_xy = [T_XYZ(1,:)./sum(T_XYZ); T_XYZ(2,:)./sum(T_XYZ)];
+    plot([T_xy(1,:) T_xy(1,1)], [T_xy(2,:) T_xy(2,1)], 'k-');
 
-% Figure stuffs.
-xlim([0 1]);
-ylim([0 1]);
-xlabel('CIE x','fontsize',13);
-ylabel('CIE y','fontsize',13);
-legend('Test','Location','southeast','fontsize',11);
-title('CIE xy coordinates');
+    % Figure stuffs.
+    xlim([0 1]);
+    ylim([0 1]);
+    xlabel('CIE x','fontsize',13);
+    ylabel('CIE y','fontsize',13);
+    legend('Test','Location','southeast','fontsize',11);
+    title('CIE xy coordinates');
 
-% CIELAB results.
-%
-% a*b* plane.
-subplot(2,2,3);
-plot(lab(:,2),lab(:,3),'r.');
-xlim([-100 100]);
-ylim([-100 100]);
-xlabel('CIELAB a*','fontsize',13);
-ylabel('CIELAB b*','fontsize',13);
-xline(0,'k-');
-yline(0,'k-');
-legend('Test','Location','southeast','fontsize',11);
-grid on;
-title('CIELAB a*b*');
+    % CIELAB results.
+    %
+    % a*b* plane.
+    subplot(2,2,3);
+    plot(lab(2,:),lab(3,:),'r.');
+    xlim([-100 100]);
+    ylim([-100 100]);
+    xlabel('CIELAB a*','fontsize',13);
+    ylabel('CIELAB b*','fontsize',13);
+    xline(0,'k-');
+    yline(0,'k-');
+    legend('Test','Location','southeast','fontsize',11);
+    grid on;
+    title('CIELAB a*b*');
 
-% L*C* plane.
-subplot(2,2,4);
-plot(lab_chroma,lab(:,1),'r.');
-xlim([0 120]);
-ylim([0 100]);
-xlabel('CIELAB C*','fontsize',13);
-ylabel('CIELAB L*','fontsize',13);
-legend('Test','Location','southeast','fontsize',11);
-title('CIELAB L*C*');
-grid on;
+    % L*C* plane.
+    subplot(2,2,4);
+    plot(lab_chroma,lab(1,:),'r.');
+    xlim([0 120]);
+    ylim([0 100]);
+    xlabel('CIELAB C*','fontsize',13);
+    ylabel('CIELAB L*','fontsize',13);
+    legend('Test','Location','southeast','fontsize',11);
+    title('CIELAB L*C*');
+    grid on;
+end
 
 %% Print out the dRGB of choices.
 
