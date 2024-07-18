56da5310865e8b4626e91bb681313dd46b8db65c
Semin <ohsemin1990@gmail.com>
2024-07-18 12:03:41 +0200

    removed unnecessary numbers

--- toolbox/CAM/rgb2xyz.m
+++ toolbox/CAM/rgb2xyz.m
@@ -3,16 +3,13 @@ rgb1 = imread(rgb);
 rgb_d = double(rgb1); %digital RGB
 [M N P] = size(rgb_d);
 
-rgb_d = 100*(((rgb_d./255)).^gamma); %linear RGB
-rgb2XYZ = [0.5805 0.1687 0.1700; 0.2766 0.6428 0.0814; 0.0099 0.1018 0.9065]; % Custom mode
-% rgb2XYZ = [0.4124 0.3576 0.1805;0.2126 0.7152 0.0722; 0.0193 0.1192 0.9505]; %sRGB
-% rgb2XYZ = [64.45 32.62 1.52;45.30 97.07 13.90;23.81 11.15 125.76]; % monitor gamut - absolute
-
+%linear RGB
+rgb_d = (((rgb_d./255)).^gamma);
+rgb2XYZ = [0.5805 0.1687 0.1700; 0.2766 0.6428 0.0814; 0.0099 0.1018 0.9065];
 
 X = rgb2XYZ(1,1).*rgb_d(:,:,1) + rgb2XYZ(1,2).*rgb_d(:,:,2) + rgb2XYZ(1,3).*rgb_d(:,:,3);
 Y = rgb2XYZ(2,1).*rgb_d(:,:,1) + rgb2XYZ(2,2).*rgb_d(:,:,2) + rgb2XYZ(2,3).*rgb_d(:,:,3);
 Z = rgb2XYZ(3,1).*rgb_d(:,:,1) + rgb2XYZ(3,2).*rgb_d(:,:,2) + rgb2XYZ(3,3).*rgb_d(:,:,3);
-
 
 XYZ = [X(:), Y(:),Z(:)]; %tristimulus value
 
--- toolbox/CAM/xyz2rgb.m
+++ toolbox/CAM/xyz2rgb.m
@@ -1,19 +1,14 @@
-function RGB_result = xyz2rgb(XYZ, M, N, result)
-
-
-rgb2XYZ = [0.5805 0.1687 0.1700; 0.2766 0.6428 0.0814; 0.0099 0.1018 0.9065]; % Custom mode
-% rgb2XYZ = [0.4124 0.3576 0.1805;0.2126 0.7152 0.0722; 0.0193 0.1192 0.9505]; %sRGB gamut
+function RGB = xyz2rgb(XYZ, M, N, result)
+rgb2XYZ = [0.5805 0.1687 0.1700; 0.2766 0.6428 0.0814; 0.0099 0.1018 0.9065]; 
 XYZ2rgb = inv(rgb2XYZ);
 
+RGB(:,1) = XYZ2rgb(1,1).* XYZ(:,1) + XYZ2rgb(1,2).* XYZ(:,2) + XYZ2rgb(1,3).* XYZ(:,3);
+RGB(:,2) = XYZ2rgb(2,1).* XYZ(:,1) + XYZ2rgb(2,2).* XYZ(:,2) + XYZ2rgb(2,3).* XYZ(:,3);
+RGB(:,3) = XYZ2rgb(3,1).* XYZ(:,1) + XYZ2rgb(3,2).* XYZ(:,2) + XYZ2rgb(3,3).* XYZ(:,3);
 
-RGB_result(:,1) = XYZ2rgb(1,1).* XYZ(:,1) + XYZ2rgb(1,2).* XYZ(:,2) + XYZ2rgb(1,3).* XYZ(:,3);
-RGB_result(:,2) = XYZ2rgb(2,1).* XYZ(:,1) + XYZ2rgb(2,2).* XYZ(:,2) + XYZ2rgb(2,3).* XYZ(:,3);
-RGB_result(:,3) = XYZ2rgb(3,1).* XYZ(:,1) + XYZ2rgb(3,2).* XYZ(:,2) + XYZ2rgb(3,3).* XYZ(:,3);
+RGB = (((RGB).^(1/2.2)).*255);
+RGB = reshape(RGB,M,N,3);
+RGB = real(RGB);
+RGB = uint8(RGB); % 8bit digital RGB
 
-
-RGB_result = ((((RGB_result)./100).^(1/2.2)).*255);
-RGB_result = reshape(RGB_result,M,N,3);
-RGB_result = real(RGB_result);
-RGB_result = uint8(RGB_result); % 8bit digital RGB
-
-imwrite(RGB_result,result);
+imwrite(RGB,result);
