1a91e583d56a2eb823ea65c75fea6b3a45059a93
Semin <ohsemin1990@gmail.com>
2024-07-25 12:05:31 +0200

    Caught the bug and the function name back the original

--- toolbox/CAM/XYZToJCH.m
+++ toolbox/CAM/XYZToJCH.m
@@ -1,4 +1,4 @@
-function JCh = XYZToJCH(XYZ_target,XYZ_white,options)
+function JCH = XYZToJCH(XYZ_target,XYZ_white,LA,options)
 % Convert the CIE XYZ values to the CIECAM02 stats.
 %
 % Syntax:
@@ -9,20 +9,21 @@ function JCh = XYZToJCH(XYZ_target,XYZ_white,options)
 %    work for either a single pixel or multiple of an image.
 %
 % Inputs:
-%    XYZ                      - CIE XYZ values of the interest. This could
+%    XYZ_target               - CIE XYZ values of the interest. This could
 %                               be a single pixel (3x1) or multiple (3xn).
-%    XYZw                     - The white point in the scene for
-%                               calculations.
+%    XYZ_white                - CIE XYZ values of the white point in the
+%                               scene for calculations. This should be the
+%                               absolute XYZ values where its Y value
+%                               equals to the luminance (cd/m2).
+%    LA                       - 
 %
 % Outputs:
-%    d
+%    CAMStats
 %
 % Optional key/value pairs:
 %    options.surround         - Set a surround condition. Choose one among
 %                               three: 'average', 'dim', and 'dark'.
 %                               Default to 'average'.
-%    LA                       - Luminance of the adapting filed in the unit
-%                               of cd/m2.
 %
 % See also:
 %    JCHToXYZ.
@@ -34,23 +35,21 @@ function JCh = XYZToJCH(XYZ_target,XYZ_white,options)
 arguments
     XYZ_target
     XYZ_white (3,1)
-    options.LA (1,1) = []
+    LA (1,1)
     options.surround = 'average'
 end
 
-% Get the size of the target.
-[R nTargets] = size(XYZ_target);
+% Get the number of the target.
+nTargets = size(XYZ_target,2);
 
 %% Viewing Condition Parameters.
 %
-% Luminance of the background. It is usually set as 20% of the luminance of
-% the adapting field for convenience (unit cd/m2).
-if isempty(option.LA)
-    options.LA = XYZ_white(2);
-    Yb = options.LA * 0.2;
-end
+% Luminance of the background. It is usually set as 20% of the white point
+% for convenience. As we normalized the white point (Y=100), Yb gonna be 20
+% cd/m2.
+Yb = 20;
 
-% Define surround condition.
+% Define surround condition. Choose one among three.
 switch options.surround
     case 'dark'
         F = 0.8;
@@ -66,9 +65,9 @@ switch options.surround
         Nc = 1.0;
 end
 
-% More parameters based on the surround and background.
-k = 1/(5*options.LA+1);
-FL = 0.2*(k^4)*(5*options.LA) + 0.1*((1-k^4)^2)*((5*options.LA)^(1/3));
+% More parameters based on the surround and adapting field.
+k = 1/(5*LA+1);
+FL = 0.2*(k^4)*(5*LA) + 0.1*((1-k^4)^2)*((5*LA)^(1/3));
 n = Yb/XYZ_white(2);
 Nbb = 0.725*(1/n)^0.2;
 Ncb = Nbb;
@@ -137,14 +136,14 @@ t = ones(1,nTargets);
 
 % Initial opponent responses.
 a = RGBpa(1,:) - 12*RGBpa(2,:)/11 + RGBpa(3,:)/11;
-b = (1/9)*(RGBpa(1,:) + RGBpa(2,:) - 2*RGBpa(3,:));
+b = (RGBpa(1,:) + RGBpa(2,:) - 2*RGBpa(3,:))/9;
 
 % Hue angle.
 for ii = 1:nTargets
     h_temp = rad2deg(atan2(b(ii),a(ii)));
 
-    % When the hue angle value is negative, we set it to a positive by adding
-    % 360 degrees.
+    % When the hue angle value is negative, we set it to a positive by
+    % adding 360 degrees.
     if h_temp < 0
         h(1,ii) = 360 + rad2deg(atan2(b(ii),a(ii)));
     else
@@ -152,22 +151,100 @@ for ii = 1:nTargets
     end
 end
 
-% Hue quadrature.
-et = (1/4)*(cos(deg2rad(h*pi/180+2))+3.8);
+%% Hue quadrature.
+%
+% Unique hue angles defined in CIECAM02. As the hue angle circles within
+% 360, so red (20.14) is equivalent with the red (380.14).
+h_Red = 20.14;
+h_Yellow = 90;
+h_Green = 164.25;
+h_Blue = 234.79;
+h_Red360 = 380.14;
+h_Unique = [h_Red h_Yellow h_Green h_Blue h_Red360];
 
+% Hue eccentricity factors.
+e_Red = 0.8;
+e_Yellow = 0.7;
+e_Green = 1;
+e_Blue = 1.2;
+e_Red360 = 0.8;
+e_Unique = [e_Red e_Yellow e_Green e_Blue e_Red360];
 
-e = ((12500/13)*Nc*Ncb)*(cos(h*(pi/180)+2)+3.8);
-t = (e.*((a.^2+b.^2).^(0.5)))./(RGBpa(:,1)+RGBpa(:,2)+(21/20)*RGBpa(:,3));
+% Unique hue quadrature;
+H_Unique = [0 100 200 300 400];
 
-% Brightness.
-A = (2*RGBpa(:,1)+RGBpa(:,2)+(1/20)*RGBpa(:,3)-0.305)*Nbb;
-Aw = (2*RGBpaw(1,1)+RGBpaw(1,2)+(1/20)*RGBpaw(1,3)-0.305)*Nbb;
+% Set the hue angle (hq) for calculation of the hue quadrature.
+for ii = 1:nTargets
+    if h(ii) < h_Unique(1)
+        hq(1,ii) = h(ii) + 360;
+    else
+        hq(1,ii) = h(ii);
+    end
+end
 
-% Lightness.
+% Get a unique hue index.
+for ii = 1:nTargets
+    if hq(ii) < h_Unique(2)
+        idx_UniqueHue(1,ii) = 1;
+    elseif hq(ii) < h_Unique(3)
+        idx_UniqueHue(1,ii) = 2;
+    elseif hq(ii) < h_Unique(4)
+        idx_UniqueHue(1,ii) = 3;
+    else
+        idx_UniqueHue(1,ii) = 4;
+    end
+end
+
+% Get two adjacent unique hues and eccentricity.
+for ii = 1:nTargets
+    % Hue angle.
+    hi(1,ii) = h_Unique(idx_UniqueHue(ii));
+    hi_next(1,ii) = h_Unique(idx_UniqueHue(ii)+1);
+    
+    % Eccentricity.
+    ei(1,ii) = e_Unique(idx_UniqueHue(ii));
+    ei_next(1,ii) = e_Unique(idx_UniqueHue(ii)+1);
+
+    % Hue quadrature.
+    Hi(1,ii) = H_Unique(idx_UniqueHue(ii));
+end
+
+% Finally, calculate the hue quadrature (H) here.
+H = Hi + (100 * (hq-hi)./ei) ./ ((hq-hi)./ei + (hi_next-hq)./ei_next);
+
+% Lightness (J).
+A  = (2*RGBpa(1,:) + RGBpa(2,:) + (1/20)*RGBpa(3,:) - 0.305)*Nbb;
+Aw = (2*RGBpaw(1) + RGBpaw(2) + (1/20)*RGBpaw(3) - 0.305)*Nbb;
 J = 100*(A/Aw).^(c*z);
 
-% Chroma.
-nTargets = (t.^(0.9)).*((J/100).^(0.5))*(1.64-0.29^n)^(0.73);
+% Brightness (Q).
+Q = (4/c)*sqrt(J/100)*(Aw+4)*FL^0.25;
+
+% Chroma (C).
+et = (1/4)*(cos(hq*pi/180+2)+3.8);
+t = (50000/13)*Nc*Ncb*et*sqrt(a.^2+b.^2)./(RGBpa(1,:)+RGBpa(2,:)+(21/20)*RGBpa(3,:));
+C = t.^(0.9) * sqrt(J/100) * (1.64-0.29^n)^0.73;
+
+% Colorfulness (M).
+M = C * FL^0.25;
+
+% Saturation (s).
+s = 100 * sqrt(M./Q);
 
 %% Print out variables here.
-JCh = [J nTargets h];
+JCH.h = h;
+JCH.H = H;
+JCH.J = J;
+JCH.Q = Q;
+JCH.C = C;
+JCH.M = M;
+JCH.h = s;
+
+% Description of each color correlate.
+JCH.describe.h = 'Hue angle (360)';
+JCH.describe.H = 'Hue quadrature (400)';
+JCH.describe.J = 'Lightness';
+JCH.describe.Q = 'Brightness';
+JCH.describe.C = 'Chroma';
+JCH.describe.M = 'Colorfulness';
+JCH.describe.s = 'Saturation';
