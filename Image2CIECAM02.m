4dec8a2f3bb679d8d8b3d3427f3d2d14ac5e25f7
Semin <ohsemin1990@gmail.com>
2024-09-05 13:43:58 +0200

    Test

--- Image2CIECAM02.m
+++ Image2CIECAM02.m
@@ -181,3 +181,4 @@ XYZ_white = (XYZ_white./XYZ_white(2)) * 100;
 % Then, calculate the CIECAM02 stats. The output 'JCH_target' contains
 % three numbers, lightness (J), chroma (C), and hue angle (h).
 JCH_target = XYZToJCH(XYZ_target,XYZ_white,LA);
+
