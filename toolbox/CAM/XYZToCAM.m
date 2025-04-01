23fed8b51e234ad4473362a45aa8996174a374cb
Semin <ohsemin1990@gmail.com>
2025-04-01 15:11:24 +0200

    Updated to include CAM16 calculations

--- toolbox/CAM/XYZToCAM.m
+++ toolbox/CAM/XYZToCAM.m
@@ -24,6 +24,11 @@ function CAMStats = XYZToCAM(XYZ_target,XYZ_white,LA,options)
 %    options.surround         - Set a surround condition. Choose one among
 %                               three: 'average', 'dim', and 'dark'.
 %                               Default to 'average'.
+%    options.whichCAM         - Decide which CAM to calculate. Default to
+%                               CAM16. 
+%    options.size             - Define the size of stimuli. This is only
+%                               applied when using CAM16 model. Default to
+%                               medium.
 %
 % See also:
 %    JCHToXYZ.
@@ -37,6 +42,8 @@ arguments
     XYZ_white (3,1)
     LA (1,1)
     options.surround = 'average'
+    options.whichCAM = 'CAM16'
+    options.size = 'medium'
 end
 
 % Get the number of the target.
@@ -54,14 +61,17 @@ Yb = 20;
 
 % Define surround condition. Choose one among three.
 switch options.surround
+    % ~1deg field (slide).
     case 'dark'
         F = 0.8;
         c = 0.525;
         Nc = 0.8;
+        % ~4deg field (TV/movie).
     case 'dim'
         F = 0.9;
         c = 0.59;
         Nc = 0.95;
+        % ~20deg field (normal room).
     case 'average'
         F = 1.0;
         c = 0.69;
@@ -79,18 +89,32 @@ z = 1.48 + sqrt(n);
 %% Chromatic Adaptation.
 %
 % Mcat02 is the matrix to convert from the XYZ to RGB for CIECAM02.
-M_CAT02 = [0.7328 0.4296 -0.1624;...
-    -0.7036 1.6975 0.0061;...
-    0.0030 0.0136 0.9834];
+switch options.whichCAM
+    case 'CIECAM02'
+        M_CAT02 = [0.7328 0.4296 -0.1624;...
+            -0.7036 1.6975 0.0061;...
+            0.0030 0.0136 0.9834];
+        M_CAT = M_CAT02;
+    case 'CAM16'
+        M_CAT16 = [0.401288,  0.650173, -0.051461;...
+            -0.250268,  1.204414,  0.045854;...
+            -0.002079,  0.048952,  0.953127];
+        M_CAT = M_CAT16;
+end
 
 % XYZ to RGB of the target.
-RGB = M_CAT02 * XYZ_target;
+RGB = M_CAT * XYZ_target;
 
 % XYZ to RGB of the white point.
-RGBw = M_CAT02 * XYZ_white;
+RGBw = M_CAT * XYZ_white;
 
 % Degree of adaptation.
 D = F*(1-(1/3.6)*exp((-LA-42)/92));
+
+% If calculating CAM16, the D value clipped on to the range 0-1.
+if strcmp(options.whichCAM,'CAM16')
+    D = max(0,min(1,D));
+end
 
 % Calculate the corresponding color of the target. Note that the part of
 % the luminance of the white point ('whitepoint(2)') is equal to 100 if
@@ -109,15 +133,25 @@ Bcw = ((XYZ_white(2)*D/RGBw(3))+(1-D))*RGBw(3);
 RGBcw = [Rcw; Gcw; Bcw];
 
 % Here, calculate it back to the XYZ.
-XYZc  = inv(M_CAT02) * RGBc;
-XYZcw = inv(M_CAT02) * RGBcw;
+XYZc  = inv(M_CAT) * RGBc;
+XYZcw = inv(M_CAT) * RGBcw;
 
 %% Post adaptation non-linear response compression.
 %
-% Calculate the R'G'B' here.
-M_HPE = [0.38971 0.68898 -0.07868;...
-    -0.22981 1.18340 0.04641;...
-    0.00000 0.00000 1.00000];
+% Calculate the R'G'B' here. Set M_HPE matrix differently over differrent
+% models.
+switch options.whichCAM
+    case 'CIECAM02'
+        M_HPE_CIECAM02 = [0.38971 0.68898 -0.07868;...
+            -0.22981 1.18340 0.04641;...
+            0.00000 0.00000 1.00000];
+        M_HPE = M_HPE_CIECAM02;
+    case 'CAM16'
+        M_HPE_CAM16 = [0.389986,  0.688859, -0.078846;...
+            -0.229646,  1.183883,  0.045762;...
+            0.000000,  0.000000,  1.000000];
+        M_HPE = M_HPE_CAM16;
+end
 
 % R'G'B' of the target.
 RGBp = M_HPE * XYZc;
@@ -125,10 +159,18 @@ RGBp = M_HPE * XYZc;
 % R'G'B' of the white point [R'w,G'w,B'w]
 RGBpw = M_HPE * XYZcw;
 
+% Set the exponent value differently over different models.
+switch options.whichCAM
+    case 'CIECAM02'
+        numExponent = 0.42;
+    case 'CAM16'
+        numExponent = 0.43;
+end
+
 % From R'G'B' to R'a,G'a,B'a of the target.
-RGBpa  = (400*(FL*RGBp/100).^(0.42))./(27.13+(FL*RGBp/100).^(0.42)) + 0.1;
+RGBpa  = (400*(FL*RGBp/100).^(numExponent))./(27.13+(FL*RGBp/100).^(numExponent)) + 0.1;
 % From R'G'B' to R'a,G'a,B'a of the white point.
-RGBpaw = (400*(FL*RGBpw/100).^(0.42))./(27.13+(FL*RGBpw/100).^(0.42)) + 0.1;
+RGBpaw = (400*(FL*RGBpw/100).^(numExponent))./(27.13+(FL*RGBpw/100).^(numExponent)) + 0.1;
 
 %% Calculate perceptual attribute correlates.
 a = ones(1,nTargets);
@@ -228,6 +270,20 @@ et = (1/4)*(cos(hq*pi/180+2)+3.8);
 t = (50000/13)*Nc*Ncb*et*sqrt(a.^2+b.^2)./(RGBpa(1,:)+RGBpa(2,:)+(21/20)*RGBpa(3,:));
 C = t.^(0.9) * sqrt(J/100) * (1.64-0.29^n)^0.73;
 
+% For CAM16, adjust chroma over size.
+if strcmp(options.whichCAM,'CAM16')
+    % CAM16 size correction factors from CIE TC1-34.
+    switch options.size
+        case 'small'
+            c_size_factor = 0.9;
+        case 'medium'
+            c_size_factor = 0.95;
+        otherwise
+            c_size_factor = 1.0;
+    end
+    C = C * c_size_factor;
+end
+
 % Colorfulness (M).
 M = C * FL^0.25;
 
@@ -237,15 +293,17 @@ s = 100 * sqrt(M./Q);
 %% Print out variables here.
 CAMStats.h = h;
 CAMStats.H = H;
+CAMStats.A = A;
 CAMStats.J = J;
 CAMStats.Q = Q;
 CAMStats.C = C;
 CAMStats.M = M;
-CAMStats.h = s;
+CAMStats.s = s;
 
 % Description of each color correlate.
 CAMStats.describe.h = 'Hue angle (360)';
 CAMStats.describe.H = 'Hue quadrature (400)';
+CAMStats.describe.A = 'Brightness';
 CAMStats.describe.J = 'Lightness';
 CAMStats.describe.Q = 'Brightness';
 CAMStats.describe.C = 'Chroma';
--- toolbox/CAM/XYZToJCH.m
+++ toolbox/CAM/XYZToJCH.m
@@ -1,5 +1,5 @@
 function JCh = XYZToJCH(XYZ_target,XYZ_white,LA,options)
-% Convert the CIE XYZ values to the CIECAM02 stats.
+% Convert XYZ into JCH using CIECAM02 or CAM16.
 %
 % Syntax: JCH = XYZToJCH(XYZ_target,XYZ_white,LA)
 %
@@ -24,6 +24,11 @@ function JCh = XYZToJCH(XYZ_target,XYZ_white,LA,options)
 %    options.surround         - Set a surround condition. Choose one among
 %                               three: 'average', 'dim', and 'dark'.
 %                               Default to 'average'.
+%    options.whichCAM         - Decide which CAM to calculate. Default to
+%                               CAM16.
+%    options.size             - Define the size of stimuli. This is only
+%                               applied when using CAM16 model. Default to
+%                               medium.
 %
 % See also:
 %    JCHToXYZ.
@@ -32,6 +37,7 @@ function JCh = XYZToJCH(XYZ_target,XYZ_white,LA,options)
 %    07/23/24       smo       - Started on it.
 %    07/25/24       smo       - Working fine. The calculation results match
 %                               with the official CIECAM02 excel file.
+%    04/01/25       smo       - Added an option to calculate CAM16 values.
 
 %% Set variables.
 arguments
@@ -39,6 +45,8 @@ arguments
     XYZ_white (3,1)
     LA (1,1)
     options.surround = 'average'
+    options.whichCAM = 'CAM16'
+    options.size = 'medium'
 end
 
 % Get the number of the target.
@@ -53,14 +61,17 @@ Yb = 20;
 
 % Define surround condition. Choose one among three.
 switch options.surround
+    % ~1deg field (slide).
     case 'dark'
         F = 0.8;
         c = 0.525;
         Nc = 0.8;
+        % ~4deg field (TV/movie).
     case 'dim'
         F = 0.9;
         c = 0.59;
         Nc = 0.95;
+        % ~20deg field (normal room).
     case 'average'
         F = 1.0;
         c = 0.69;
@@ -77,16 +88,25 @@ z = 1.48 + sqrt(n);
 
 %% Chromatic Adaptation.
 %
-% Mcat02 is the matrix to convert from the XYZ to RGB for CIECAM02.
-M_CAT02 = [0.7328 0.4296 -0.1624;...
-    -0.7036 1.6975 0.0061;...
-    0.0030 0.0136 0.9834];
+% M_CAT is the matrix to convert from the XYZ to RGB.
+switch options.whichCAM
+    case 'CIECAM02'
+        M_CAT02 = [0.7328 0.4296 -0.1624;...
+            -0.7036 1.6975 0.0061;...
+            0.0030 0.0136 0.9834];
+        M_CAT = M_CAT02;
+    case 'CAM16'
+        M_CAT16 = [0.401288,  0.650173, -0.051461;...
+            -0.250268,  1.204414,  0.045854;...
+            -0.002079,  0.048952,  0.953127];
+        M_CAT = M_CAT16;
+end
 
 % XYZ to RGB of the target.
-RGB = M_CAT02 * XYZ_target;
+RGB = M_CAT * XYZ_target;
 
 % XYZ to RGB of the white point.
-RGBw = M_CAT02 * XYZ_white;
+RGBw = M_CAT * XYZ_white;
 
 % Degree of adaptation.
 D = F*(1-(1/3.6)*exp((-LA-42)/92));
@@ -108,15 +128,24 @@ Bcw = ((XYZ_white(2)*D/RGBw(3))+(1-D))*RGBw(3);
 RGBcw = [Rcw; Gcw; Bcw];
 
 % Here, calculate it back to the XYZ.
-XYZc  = inv(M_CAT02) * RGBc;
-XYZcw = inv(M_CAT02) * RGBcw;
+XYZc  = inv(M_CAT) * RGBc;
+XYZcw = inv(M_CAT) * RGBcw;
 
 %% Post adaptation non-linear response compression.
 %
 % Calculate the R'G'B' here.
-M_HPE = [0.38971 0.68898 -0.07868;...
-    -0.22981 1.18340 0.04641;...
-    0.00000 0.00000 1.00000];
+switch options.whichCAM
+    case 'CIECAM02'
+        M_HPE_CIECAM02 = [0.38971 0.68898 -0.07868;...
+            -0.22981 1.18340 0.04641;...
+            0.00000 0.00000 1.00000];
+        M_HPE = M_HPE_CIECAM02;
+    case 'CAM16'
+        M_HPE_CAM16 = [0.389986,  0.688859, -0.078846;...
+            -0.229646,  1.183883,  0.045762;...
+            0.000000,  0.000000,  1.000000];
+        M_HPE = M_HPE_CAM16;
+end
 
 % R'G'B' of the target.
 RGBp = M_HPE * XYZc;
@@ -124,10 +153,18 @@ RGBp = M_HPE * XYZc;
 % R'G'B' of the white point [R'w,G'w,B'w]
 RGBpw = M_HPE * XYZcw;
 
+% Set the exponent value differently over different models.
+switch options.whichCAM
+    case 'CIECAM02'
+        numExponent = 0.42;
+    case 'CAM16'
+        numExponent = 0.43;
+end
+
 % From R'G'B' to R'a,G'a,B'a of the target.
-RGBpa  = (400*(FL*RGBp/100).^(0.42))./(27.13+(FL*RGBp/100).^(0.42)) + 0.1;
+RGBpa  = (400*(FL*RGBp/100).^(numExponent))./(27.13+(FL*RGBp/100).^(numExponent)) + 0.1;
 % From R'G'B' to R'a,G'a,B'a of the white point.
-RGBpaw = (400*(FL*RGBpw/100).^(0.42))./(27.13+(FL*RGBpw/100).^(0.42)) + 0.1;
+RGBpaw = (400*(FL*RGBpw/100).^(numExponent))./(27.13+(FL*RGBpw/100).^(numExponent)) + 0.1;
 
 %% Calculate perceptual attribute correlates.
 a = ones(1,nTargets);
@@ -202,7 +239,7 @@ for ii = 1:nTargets
     % Hue angle.
     hi(1,ii) = h_Unique(idx_UniqueHue(ii));
     hi_next(1,ii) = h_Unique(idx_UniqueHue(ii)+1);
-    
+
     % Eccentricity.
     ei(1,ii) = e_Unique(idx_UniqueHue(ii));
     ei_next(1,ii) = e_Unique(idx_UniqueHue(ii)+1);
@@ -227,6 +264,20 @@ et = (1/4)*(cos(hq*pi/180+2)+3.8);
 t = (50000/13)*Nc*Ncb*et.*sqrt(a.^2+b.^2)./(RGBpa(1,:)+RGBpa(2,:)+(21/20)*RGBpa(3,:));
 C = t.^(0.9) .* sqrt(J/100) * (1.64-0.29^n)^0.73;
 
+% For CAM16, adjust chroma over size.
+if strcmpi(options.whichCAM,'CAM16')
+    % CAM16 size correction factors from CIE TC1-34.
+    switch options.size
+        case 'small'
+            c_size_factor = 0.9;
+        case 'medium'
+            c_size_factor = 0.95;
+        otherwise
+            c_size_factor = 1.0;
+    end
+    C = C * c_size_factor;
+end
+
 % Colorfulness (M).
 M = C * FL^0.25;
 
@@ -246,7 +297,7 @@ JCh = [J; C; h];
 % JCH.C = C;
 % JCH.M = M;
 % JCH.s = s;
-% 
+%
 % % Description of each color correlate.
 % JCH.describe.h = 'Hue angle (360)';
 % JCH.describe.H = 'Hue quadrature (400)';
