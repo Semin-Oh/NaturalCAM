4d209a2ec3ddb74a6bb521e51e2af375faa2d4d5
Semin <seminoh@sas.upenn.edu>
2025-04-11 17:23:05 +0200

    New script to test out some image color stuff

---
+++ analysis/NCAM_ImageAnalysis.m
@@ -0,0 +1,69 @@
+% NCAM_ImageAnalysis.
+% 
+% This routine tests out some color stuff on the images. This will be
+% eventually added to the data analysis routine.
+%
+% See also:
+%    NCAM_DataAnalysis.m.
+
+% History:
+%    04/11/25    smo       - Started on it.
+
+%% Initialize.
+clear; close all;
+
+%% Set variables.
+%
+% Set directory.
+sysInfo = GetComputerInfo();
+switch sysInfo.userShortName
+    % Semin office computer.
+    case 'semin'
+        baseFiledir = '/Users/semin/Dropbox (Personal)/JLU/2) Projects';
+        % Lab Linux computer.
+    case 'gegenfurtner'
+        baseFiledir = '/home/gegenfurtner/Dropbox/JLU/2) Projects';
+    otherwise
+        % Semin's laptop.
+        baseFiledir = 'C:\Users\ohsem\Dropbox (Personal)\JLU\2) Projects';
+end
+projectName = 'NaturalCAM';
+imageFiledir = fullfile(baseFiledir,projectName,'images','segmentation','images_labeled');
+segmentationFiledir = fullfile(baseFiledir,projectName,'images','segmentation','segmentation_labeled');
+
+% Get available image names.
+imageFileList = dir(imageFiledir);
+imageNameList = {imageFileList.name};
+imageNameOptions = imageNameList(~startsWith(imageNameList,'.'));
+
+% Get available segmentation names.
+segFileList = dir(segmentationFiledir);
+segNameList = {segFileList.name};
+segNameOptions = segNameList(~startsWith(segNameList,'.'));
+
+
+% Display parameters.
+displayType = 'EIZO';
+switch displayType
+    case 'EIZO'
+        M_RGBToXYZ =  [62.1997 22.8684 19.2310;...
+            28.5133 78.5446 6.9256;...
+            0.0739 6.3714 99.5962];
+        gamma_display = 2.1904;
+
+        XYZ_white = sum(M_RGBToXYZ,2);
+end
+
+%% Check out the image segmentations.
+numImage = 6;
+imageFilename = imageNameOptions{numImage};
+image = imread(fullfile(imageFiledir,imageFilename));
+
+% Read out segmentation data. 
+segFilename = segNameOptions{numImage};
+fid = fopen(fullfile(segmentationFiledir,segFilename),"r");
+segmentData = textscan(fid, '%f %s %f %f %f %f %f', 'Delimiter', ',', 'HeaderLines', 1);
+fclose(fid);
+
+% Get dominant color.
+XYZ_dominantColor = GetImageDominantColor(image,segmentData,M_RGBToXYZ,gamma_display,XYZ_white);
